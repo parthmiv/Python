73cecb6c1a39062ec393c9376a3b0cd4df1d2f17
parthmiv <122524279+parthmiv@users.noreply.github.com>
2024-08-25 23:42:09 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,15 @@
+print("Hello World")
+print("how are you?")
+# triple single or double quotes are used to print multiple lines
+print('''Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!
+Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!''')
